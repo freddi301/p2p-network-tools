b74317030cd33644e1431b474a6a884b251399f8
Frederik Batuna <gobi301@gmail.com>
2020-10-02 00:56:24 +0200

    desktop UI rewamp (now it's cool)

--- desktop-explorer-electron/src/gui.tsx
+++ desktop-explorer-electron/src/gui.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { css, injectGlobal } from "emotion";
 import { Hash, hashFromBlock } from "p2p-network-tools-node-nodejs";
+import { ipcRenderer } from "electron";
 
 type Props = {
   subscribeBlock(
@@ -10,13 +11,6 @@ type Props = {
   ): { unsubscribe(): void };
   provideBlock(block: Buffer): void;
 };
-
-injectGlobal`
-  body {
-    margin: 0;
-  }
-  @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400&display=swap');
-`;
 
 export function start(props: Props) {
   ReactDOM.render(
@@ -35,7 +29,7 @@ function App({ subscribeBlock, provideBlock }: Props) {
     const { unsubscribe } = subscribeBlock(hash, (block) => {
       // console.log(hash.toString("hex"), block.toString());
     });
-    setTabs([{ hash, unsubscribe }, ...tabs]);
+    setTabs((tabs) => [{ hash, unsubscribe }, ...tabs]);
   };
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
   const selectedTab = tabs[selectedTabIndex];
@@ -64,272 +58,337 @@ function App({ subscribeBlock, provideBlock }: Props) {
       className={css`
         width: 100vw;
         height: 100vh;
+        overflow: hidden;
         background-color: ${colors.background};
         color: ${colors.white};
         font-family: "Fira Code", monospace;
         display: grid;
-        grid-template-columns: 300px auto;
-        grid-template-rows: auto 1fr;
-        grid-template-areas:
-          "side status"
-          "side main";
+        grid-template-columns: [side] 250px [main] auto;
+        grid-template-rows: [top] auto [center] 1fr [end];
       `}
     >
       <div
+        onClick={() => {
+          setSelectedTabIndex(NaN);
+        }}
         className={css`
-          grid-area: side;
+          grid-column: side;
+          grid-row: top;
+          cursor: default;
+          padding: 0.5em;
+          background-color: ${!selectedTab
+            ? colors.backgroundHover
+            : colors.backgroundDark};
+          &:hover {
+            background-color: ${colors.backgroundHover};
+          }
+          text-align: center;
+          transition: 0.3s;
+          user-select: none;
+        `}
+      >
+        <span role="img" aria-label="home">
+          home
+        </span>
+      </div>
+      <div
+        className={css`
+          grid-column: side;
+          grid-row: center;
           background-color: ${colors.backgroundDark};
+          overflow-y: auto;
+          height: 100%;
+          position: relative;
         `}
       >
         <div
-          onClick={() => {
-            setSelectedTabIndex(NaN);
-          }}
           className={css`
-            cursor: default;
-            padding: 0ch 1ch;
-            background-color: ${!selectedTab
-              ? colors.backgroundHover
-              : "inherit"};
-            &:hover {
-              background-color: ${colors.backgroundHover};
-            }
-            text-align: center;
+            position: absolute;
+            width: 100%;
           `}
         >
-          <span role="img" aria-label="home">
-            🏠
-          </span>
-        </div>
-        {tabs.map((tab, index) => {
-          const isOpen = index === selectedTabIndex;
-          return (
-            <div
-              key={index}
-              className={css`
-                padding: 0ch 1ch;
-                cursor: default;
-                background-color: ${isOpen
-                  ? colors.backgroundHover
-                  : "inherit"};
-                &:hover {
-                  background-color: ${colors.backgroundHover};
-                  .hover-action {
-                    visibility: visible;
-                  }
-                }
-                position: relative;
-                display: flex;
-              `}
-            >
+          {tabs.map((tab, index) => {
+            const isOpen = index === selectedTabIndex;
+            return (
               <div
+                key={index}
                 onClick={() => {
                   setSelectedTabIndex(index);
                 }}
                 className={css`
-                  flex-grow: 1;
+                  padding: 0.5em;
+                  cursor: default;
+                  background-color: ${isOpen
+                    ? colors.backgroundHover
+                    : "inherit"};
+                  &:hover {
+                    background-color: ${colors.backgroundHover};
+                  }
+                  display: flex;
+                  transition: 0.3s;
                 `}
               >
                 <div
                   className={css`
-                    width: 200px;
+                    color: ${colors.gray};
+                    padding-right: 0.5em;
+                  `}
+                >
+                  #
+                </div>
+
+                <div
+                  className={css`
+                    flex-grow: 1;
                     overflow: hidden;
                     text-overflow: ellipsis;
                   `}
                 >
-                  <span
-                    className={css`
-                      color: ${colors.gray};
-                    `}
-                  >
-                    #
-                  </span>
                   {tab.hash.toString("hex")}
                 </div>
               </div>
-              <div
-                className={
-                  "hover-action " +
-                  css`
-                    position: absolute;
-                    top: 0px;
-                    right: 0px;
-                    visibility: ${isOpen ? "visible" : "hidden"};
-                    padding: 0ch 1ch;
-                    background-color: "inherit";
-                  `
-                }
-                onClick={() => {
-                  tab.unsubscribe();
-                  setTabs(tabs.filter((t, i) => i !== index));
-                }}
-              >
-                x
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-      <div
-        className={css`
-          grid-area: status;
-          background-color: ${colors.backgroundDark};
-          padding: 0ch 1ch;
-        `}
-      >
-        {selectedTab && (
-          <>
+      {selectedTab && (
+        <div
+          className={css`
+            grid-column: main;
+            grid-row: top;
+            background-color: ${colors.backgroundDark};
+            display: flex;
+          `}
+        >
+          <div
+            className={css`
+              color: ${colors.gray};
+              padding: 0.5em 0 0.5em 1em;
+            `}
+          >
+            #
+          </div>
+          <div
+            className={css`
+              padding: 0.5em;
+            `}
+          >
+            {selectedTab.hash.toString("hex")}
+          </div>
+          <div style={{ flexGrow: 1 }} />
+          <div
+            className={css`
+              background-color: ${colors.backgroundDark};
+              &:hover {
+                background-color: ${colors.backgroundHover};
+              }
+              user-select: none;
+              cursor: default;
+              padding: 0.5em;
+              justify-self: flex-end;
+            `}
+            onClick={() => {
+              selectedTab.unsubscribe();
+              setTabs(tabs.filter((t, i) => i !== selectedTabIndex));
+            }}
+          >
+            delete
+          </div>
+        </div>
+      )}
+      {selectedTab ? (
+        <div
+          className={css`
+            grid-column: main;
+            grid-row: center;
+            overflow: auto;
+            position: relative;
+            height: 100%;
+            box-sizing: border-box;
+          `}
+        >
+          <div
+            className={css`
+              position: absolute;
+              padding: 1em;
+              white-space: pre-wrap;
+            `}
+          >
+            {content ? (
+              content.toString()
+            ) : (
+              <span
+                className={css`
+                  color: ${colors.gray};
+                `}
+              >
+                Looking for somebody with this stuff that is online ...
+              </span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div
+          className={css`
+            grid-column: main;
+            grid-row: top / span 2;
+            display: flex;
+            width: 100%;
+            height: 100%;
+            padding: 0 1em;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <p
+            className={css`
+              width: 100%;
+              text-align: center;
+              -webkit-app-region: drag;
+              user-select: none;
+            `}
+          >
+            This app lets you share text over internet or wifi
+            <br />
+            No account or server needed
+            <br />
             <span
               className={css`
                 color: ${colors.gray};
               `}
             >
-              #
+              Both of you must be online to make it work
             </span>
-            <span>{selectedTab.hash.toString("hex")}</span>
-          </>
-        )}
-      </div>
-      <div
-        className={css`
-          grid-area: main;
-        `}
-      >
-        {selectedTab ? (
-          <div
-            className={css`
-              padding: 1ch;
-            `}
-          >
-            {content?.toString()}
-          </div>
-        ) : (
+            <br />
+          </p>
+
           <div
             className={css`
               display: flex;
-              justify-content: center;
+              flex-direction: row-reverse;
+              margin-bottom: 1em;
             `}
           >
             <div
               className={css`
-                width: 100%;
-                padding: 0 1ch;
+                cursor: default;
+                padding: 0.5em;
+                background-color: ${colors.backgroundDark};
+                &:hover {
+                  background-color: ${colors.backgroundHover};
+                }
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                user-select: none;
+                transition: 0.3s;
+              `}
+              onClick={() => {
+                openHash();
+              }}
+            >
+              open
+            </div>
+            <input
+              placeholder="paste"
+              value={hashText}
+              onChange={(event) => setHashText(event.currentTarget.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  openHash();
+                }
+              }}
+              className={css`
+                flex-grow: 1;
+                padding: 0.5em;
+                background-color: ${colors.backgroundDark};
+                color: inherit;
+                font-family: inherit;
+                font-size: inherit;
+                border: 0;
+                outline: 2px dashed ${colors.backgroundLight};
+                &:focus {
+                  outline: 2px dashed ${colors.blue};
+                }
+              `}
+            />
+          </div>
+          <div
+            className={css`
+              margin-bottom: 1em;
+              flex-grow: 1;
+              display: flex;
+              flex-direction: column;
+            `}
+          >
+            <div
+              onClick={() => {
+                const block = Buffer.from(editText);
+                const hash = hashFromBlock(block);
+                addTab(hash);
+                setSelectedTabIndex(0);
+                provideBlock(block);
+                setEditText("");
+              }}
+              className={css`
+                cursor: default;
+                padding: 0.5em;
+                background-color: ${colors.backgroundDark};
+                &:hover {
+                  background-color: ${colors.backgroundHover};
+                }
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                user-select: none;
+                transition: 0.3s;
               `}
             >
-              <p
-                className={css`
-                  text-align: center;
-                `}
-              >
-                This app lets you share read-only text over internet or your
-                wifi.
-                <br />
-                No account or server needed.
-                <br />
-              </p>
-              <div
-                className={css`
-                  display: flex;
-                  margin: 1em 0;
-                  flex-direction: row-reverse;
-                `}
-              >
-                <div
-                  className={css`
-                    cursor: default;
-                    padding: 0.5em;
-                    background-color: ${colors.backgroundDark};
-                    &:hover {
-                      background-color: ${colors.backgroundHover};
-                    }
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                  `}
-                  onClick={() => {
-                    openHash();
-                  }}
-                >
-                  open
-                </div>
-                <input
-                  placeholder="paste hash"
-                  value={hashText}
-                  onChange={(event) => setHashText(event.currentTarget.value)}
-                  onKeyDown={(event) => {
-                    if (event.key === "Enter") {
-                      openHash();
-                    }
-                  }}
-                  className={css`
-                    flex-grow: 1;
-                    padding: 0.5em;
-                    background-color: ${colors.backgroundDark};
-                    color: inherit;
-                    font-family: inherit;
-                    font-size: inherit;
-                    border: 0;
-                    outline: 2px dashed ${colors.backgroundLight};
-                    &:focus {
-                      outline: 2px dashed ${colors.blue};
-                    }
-                  `}
-                />
-              </div>
-              <div
-                className={css`
-                  margin: 1em 0;
-                  flex-grow: 1;
-                `}
-              >
-                <textarea
-                  value={editText}
-                  onChange={(event) => setEditText(event.currentTarget.value)}
-                  placeholder="write something smart"
-                  className={css`
-                    width: 100%;
-                    min-height: 400px;
-                    padding: 1em;
-                    resize: none;
-                    background-color: ${colors.backgroundDark};
-                    color: inherit;
-                    font-family: inherit;
-                    font-size: inherit;
-                    border: 0;
-                    outline: 2px dashed ${colors.backgroundLight};
-                    &:focus {
-                      outline: 2px dashed ${colors.blue};
-                    }
-                  `}
-                />
-                <div
-                  onClick={() => {
-                    const block = Buffer.from(editText);
-                    const hash = hashFromBlock(block);
-                    addTab(hash);
-                    setSelectedTabIndex(0);
-                    provideBlock(block);
-                    setEditText("");
-                  }}
-                  className={css`
-                    cursor: default;
-                    padding: 0.5em;
-                    background-color: ${colors.backgroundDark};
-                    &:hover {
-                      background-color: ${colors.backgroundHover};
-                    }
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                  `}
-                >
-                  save
-                </div>
-              </div>
+              save
             </div>
+            <textarea
+              value={editText}
+              onChange={(event) => setEditText(event.currentTarget.value)}
+              placeholder="write something smart"
+              className={css`
+                width: 100%;
+                flex-grow: 1;
+                padding: 0.5em;
+                resize: none;
+                background-color: ${colors.backgroundDark};
+                color: inherit;
+                font-family: inherit;
+                font-size: inherit;
+                border: 0;
+                outline: 2px dashed ${colors.backgroundLight};
+                &:focus {
+                  outline: 2px dashed ${colors.blue};
+                }
+              `}
+            />
           </div>
-        )}
-      </div>
+        </div>
+      )}
+      {!selectedTab && (
+        <div
+          onClick={() => {
+            ipcRenderer.send("app:quit");
+          }}
+          className={css`
+            background-color: ${colors.red};
+            color: ${colors.background};
+            padding: 0.5em;
+            position: fixed;
+            top: 0;
+            right: 1em;
+            user-select: none;
+            -webkit-app-region: no-drag;
+          `}
+        >
+          exit
+        </div>
+      )}
     </div>
   );
 }
@@ -347,3 +406,20 @@ const colors = {
   yellow: "#e5c07b",
   green: "#98c379",
 };
+
+injectGlobal`
+  body {
+    margin: 0;
+  }
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400&display=swap');
+  ::-webkit-scrollbar {
+    background-color: ${colors.backgroundDark};
+    width: 6px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${colors.backgroundFocus}
+  }
+  ::-webkit-scrollbar-corner {
+    background-color: ${colors.backgroundDark};
+  }
+`;
